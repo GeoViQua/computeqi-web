a50d75fa1d45b86ea52aae3b1cf2556a512eed79
Richard Jones <itszootime@gmail.com>
2012-07-05 12:01:45 +0100

    add defaults to emulator, help text on form

--- app/models/emulator.rb
+++ app/models/emulator.rb
@@ -41,6 +41,10 @@ class Emulator
 
   validates_presence_of :nugget_variance, if: Proc.new { self.nugget_variance_enabled }
   validates_numericality_of :nugget_variance, if: Proc.new { self.nugget_variance_enabled }
+
+  def calculate_defaults
+    self.training_size = self.emulator_project.design.size * 0.33
+  end
   
   def to_hash
     # get project and spec
