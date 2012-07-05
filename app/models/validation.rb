b2c063ea3d9426936d0f9d40fb8f8bca529c8169
Richard Jones <itszootime@gmail.com>
2012-07-05 14:24:17 +0100

    add design split to validation

--- app/models/validation.rb
+++ app/models/validation.rb
@@ -6,12 +6,21 @@ class Validation
   has_many :validation_values
   
   field :design_size, type: Integer
+
   field :emulator_time, type: Integer
-  field :simulator_time, type: Integer
+
+  has_one :design, as: :designable
+  has_one :run, as: :runnable
+
+  # validation needs to check that design_size is not greater than emulator.validation_indicies.size
+  # 
+
+  def calculate_defaults
+    self.design_size = self.emulator_project.emulator.validation_indices.size
+  end
   
   def to_hash
     { emulatorTime: self.emulator_time,
-      simulatorTime: self.simulator_time,
       outputResults: self.validation_values.collect {|value| value.to_hash } }
   end
   
@@ -19,18 +28,31 @@ class Validation
     # get project and spec
     project = self.emulator_project
     spec = project.simulator_specification
+    emulator = project.emulator
     
     # get emulator hash
-    emulator_hash = project.emulator.to_hash
+    emulator_hash = emulator.to_hash
+
+    # existing
+    full_design = project.design
+    full_run = project.run
+
+    # select design and run
+    indices = self.emulator_project.emulator.validation_indices
+    self.design = self.create_design(simulator_specification: spec, size: self.design_size)
+    full_design.design_values.each do |dv|
+      self.design.design_values.create(input: dv.input, points: indices.collect {|i| dv.points[i] })
+    end
+
+    self.run = self.create_run(simulator_specification: spec, design: self.design, size: self.design_size)
+    selected_run = full_run.run_values.where(output_id: emulator.output.id).first
+    self.run.run_values.create(output: emulator.output, points: indices.collect {|i| selected_run.points[i] })
     
     # request hash
     { type: 'ValidationRequest',
-      serviceURL: spec.service_url,
-      processIdentifier: spec.process_name,
       emulator: emulator_hash,
-      inputs: emulator_hash[:inputs],
-      outputs: emulator_hash[:outputs],
-      designSize: self.design_size }
+      design: self.design.to_hash,
+      evaluationResult: self.run.to_hash }
   end
   
   def handle(response)
@@ -41,7 +63,6 @@ class Validation
     # parse result
     result = response['result']
     self.emulator_time = result['emulatorTime']
-    self.simulator_time = result['processTime']
     result['outputResults'].each do |set|
       output = outputs.where(:name => set['outputIdentifier']).first
       self.validation_values.create(output: output, z_scores: set['zScores'], simulator: set['simulator'],
