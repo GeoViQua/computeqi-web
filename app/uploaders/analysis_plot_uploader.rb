bf0c3a250d20ae455f16089a412d91530ea4db50
Richard Jones <itszootime@gmail.com>
2012-07-11 09:01:28 +0100

    nothing really

--- app/uploaders/analysis_plot_uploader.rb
+++ app/uploaders/analysis_plot_uploader.rb
@@ -11,9 +11,9 @@ class AnalysisPlotUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  Provide
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # end a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
