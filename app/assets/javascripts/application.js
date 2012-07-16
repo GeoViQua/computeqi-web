48474cbcfe06871abfe2ee178df34799517ba460
Richard Jones <itszootime@gmail.com>
2012-07-16 14:01:01 +0100

    spinny emulator project creation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,12 +5,29 @@
 // the compiled file.
 //
 //= require jquery_ujs
+//= require twitter/bootstrap/button
 //= require twitter/bootstrap/dropdown
 //= require twitter/bootstrap/tab
 //= require twitter/bootstrap/tooltip
-//= require twitter/bootstrap/button
+//= require spin.min
 //= require emulatorization
 //= require_self
+
+$.fn.spin = function(opts) {
+  this.each(function() {
+    var $this = $(this),
+        data = $this.data();
+
+    if (data.spinner) {
+      data.spinner.stop();
+      delete data.spinner;
+    }
+    if (opts !== false) {
+      data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+    }
+  });
+  return this;
+};
 
 $(function() {
   
