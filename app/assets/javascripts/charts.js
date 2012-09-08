d26d36e34b180c60d4c7beb0c4da47b7960744e9
Richard Jones <itszootime@gmail.com>
2012-09-08 13:38:13 +0100

    1-to-1 lines are back

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -353,12 +353,11 @@ function createValidationOutputPlot(container, json) {
       axisLabel: 'Emulator output',
     }]
   };
-
-  //{ lines: { show: true }, data: [[xmin, ymin],[xmax, ymax]]},
   
   // create plot
   var element = $('#' + container);
   var plot = $.plot(element, [
+      { lines: { show: true }, data: [[xmin, ymin],[xmax, ymax]]},
       { points: points, data: data }
     ], options);
 
