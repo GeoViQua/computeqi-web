7e1d73f6b4204474b498bf7932650887514c4eba
Richard Jones <itszootime@gmail.com>
2012-12-05 22:46:24 +0000

    add new validation tests for ensemble predictions

--- spec/models/validation_spec.rb
+++ spec/models/validation_spec.rb
@@ -39,6 +39,26 @@ describe Validation do
     it "should have key :predicted" do
       @request_hash.should have_key(:predicted)
     end
+
+    it "should have array for :predicted value" do
+      @request_hash[:predicted].class.should == Array
+    end
+
+    describe "with predicted ensembles" do
+      before do
+        # quick (and confusing) way to add ensembles
+        @validation.predicted = [[1,2,3],[1,2,3]]
+        @request_hash = @validation.generate
+      end
+
+      it "should have hash for :predicted value" do
+        @request_hash[:predicted].class.should == Hash
+      end
+
+      it "should have :members key in :predicted hash" do
+        @request_hash[:predicted].should have_key(:members)
+      end
+    end
   end
 
   describe "after remote request" do
