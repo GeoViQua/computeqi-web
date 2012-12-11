b73f85f25250ca1f5ca1bca08df4ecf131036b69
Richard Jones <itszootime@gmail.com>
2012-12-11 15:15:40 +0000

    validation csv import

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -3,23 +3,30 @@ class ValidationsController < Remote::RemotableController
   alias :super_create :create
 
   def create
-    observed_raw = params[:validation][:observed]
-    predicted_raw = params[:validation][:predicted]
+    observed_hash = params[:validation][:observed]
+    predicted_hash = params[:validation][:predicted]
 
-    observed_hash = Hash[observed_raw[:ids].zip observed_raw[:values]]
-    predicted_hash = Hash[predicted_raw[:ids].zip predicted_raw[:values]]
-    
     observed = Array.new
     predicted = Array.new
 
     observed_hash.each do |id, value|
-      observed << value.to_f
-      predicted << predicted_hash[id].split(',').map {|value| value.to_f }
+      observed << parse_value(value)
+      predicted << parse_value(predicted_hash[id])
     end
 
     params[:validation][:observed] = observed
     params[:validation][:predicted] = predicted
     
     super_create
+  end
+
+  private
+
+  def parse_value(value)
+    begin
+      JSON.parse(value)
+    rescue JSON::ParserError
+      value.to_f
+    end
   end
 end
