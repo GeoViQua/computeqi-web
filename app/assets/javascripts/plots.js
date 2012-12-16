5af627c552a241f3067545c6b77720f528a4f17b
Richard Jones <itszootime@gmail.com>
2012-12-16 22:28:08 +0000

    adding more plots, tidying up labels

--- app/assets/javascripts/plots.js
+++ app/assets/javascripts/plots.js
@@ -6,8 +6,14 @@ $e.plot = function($container, type, data) {
     case 'mean_residual_histogram':
       $e.plotResidualHistogram($container, data, 'mean');
       break;
+    case 'mean_residual_qq_plot':
+      $e.plotResidualQQ($container, data, 'mean');
+      break;
     case 'median_residual_histogram':
       $e.plotResidualHistogram($container, data, 'median');
+      break;
+    case 'median_residual_qq_plot':
+      $e.plotResidualQQ($container, data, 'median');
       break;
     case 'reliability_diagram':
       $e.plotReliabilityDiagram($container, data);
@@ -26,6 +32,28 @@ $e.baseParse = function(data) {
     array.push([x[i], y[i]]);
   }
   return array;
+};
+
+$e.calculateMinMax = function(data) {
+  var minX;
+  var maxX;
+  var minY;
+  var maxX;
+  for (var i = 0; i < data.x.length; i++) {
+    if (i == 0 || data.x[i] < minX) {
+      minX = data.x[i];
+    }
+    if (i == 0 || data.x[i] > maxX) {
+      maxX = data.x[i];
+    }
+    if (i == 0 || data.y[i] < minY) {
+      minY = data.y[i];
+    }
+    if (i == 0 || data.y[i] > maxX) {
+      maxY = data.y[i];
+    }
+  }
+  return { minX: minX, maxX: maxX, minY: minY, maxY: maxY };
 }
 
 $e.basePlot = function($container, data, options, formatter) {
@@ -93,7 +121,7 @@ $e.basePlot = function($container, data, options, formatter) {
 $e.plotStandardScore = function($container, data) {
   // create data
   var pdata = [
-    { label: 'Standard score',
+    { label: 'k+',
       data: $e.baseParse(data),
       points: {
         show: true,
@@ -125,10 +153,11 @@ $e.plotStandardScore = function($container, data) {
 $e.plotResidualHistogram = function($container, data, source) {
   // create data
   var pdata = [
-    { label: 'Frequency',
+    { label: 'k+',
       data: $e.baseParse(data),
-      bars: { show: true, align: 'center' } }
+      bars: { show: true, align: 'center', barWidth: 0.03 } }
   ];
+  console.log(pdata);
 
   // create options
   var options = {
@@ -146,15 +175,17 @@ $e.plotResidualHistogram = function($container, data, source) {
 $e.plotReliabilityDiagram = function($container, data) {
   // create data
   var pdata = [
-    { label: 'Observed frequency',
-      data: $e.baseParse(data),
-      points: { show: true, radius: 5 } },
-    { label: 'Identity',
+    { label: 'r',
       data: [[0,0],[1,1]],
       lines: { show: true },
-      hoverable: false }
+      color: '#afd8f8',
+      hoverable: false },
+    { label: 'k+',
+      data: $e.baseParse(data),
+      points: { show: true, radius: 5 },
+      color: '#edc240' }
   ];
-
+      
   // create options
   var options = {
     yaxis: { min: 0, max: 1 },
@@ -169,11 +200,41 @@ $e.plotReliabilityDiagram = function($container, data) {
   var plot = $e.basePlot($container, pdata, options);
 
   // add data labels
-  $.each(pdata[0].data, function(i, xy) {
+  $.each(pdata[1].data, function(i, xy) {
     var offset = plot.pointOffset({ x: xy[0], y: xy[1] });
     var $label = $('<div class="plot-label"></div>');
     $label.html(data.n[i]); // lookup from original data
     $label.css({ left: offset.left + 10, top: offset.top });
     $label.appendTo(plot.getPlaceholder());
   });
+};
+
+$e.plotResidualQQ = function($container, data, source) {
+  // create data
+  var minMax = $e.calculateMinMax(data);
+  var pdata = [
+    { label: 'r',
+      data: [[minMax.minX,minMax.minY],[minMax.maxX,minMax.maxY]],
+      lines: { show: true },
+      color: '#afd8f8',
+      hoverable: false },
+    { label: 'k+',
+      data: $e.baseParse(data),
+      points: { show: true, radius: 5 },
+      color: '#edc240' }
+  ];
+
+  // create options
+  var options = {
+    xaxis: { min: minMax.xMin, max: minMax.xMax },
+    yaxis: { min: minMax.yMin, max: minMax.yMax },
+    xaxes: [{
+      axisLabel: 'Observed residual quantiles'
+    }],
+    yaxes: [{
+      axisLabel: 'Predicted ' + source + ' residual quantiles',
+    }]
+  };
+
+  $e.basePlot($container, pdata, options);
 };
