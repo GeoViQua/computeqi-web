0c5e0af98f00b1ef11ceaca65c7c6c9cccd4f8d9
Richard Jones <itszootime@gmail.com>
2013-04-08 16:55:03 +0200

    more cancan coverage

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, EmulatorProject, :user_id => user.id
+    can :manage, SensitivityProject, :user_id => user.id
+    can :manage, ValidationProject, :user_id => user.id
   end
 end
