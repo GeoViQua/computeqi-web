c68b29e96ac53a0bbfa1af747d1236e9fd65cda2
Richard Jones <itszootime@gmail.com>
2013-04-09 09:47:18 +0200

    building spec for input controller

--- spec/controllers/inputs_controller_spec.rb
+++ spec/controllers/inputs_controller_spec.rb
@@ -5,18 +5,49 @@ describe InputsController do
   # show json, put js
   # nested simulator_specification_id/id
 
-  describe 'GET #show'
+  before do
+    @project = FactoryGirl.create(:trained_ep)
+    @spec = @project.simulator_specification
+    @input = @spec.inputs.first
+    sign_in @project.user
+  end
+
+  describe 'GET #show' do
     context 'as owner' do
+      before do
+        get :show, format: :json, emulator_project_id: @project, simulator_specification_id: @spec, id: @input.name
+      end
+
+      it 'assigns the correct input' do
+        assigns(:input).should eq(@input)
+      end
+
+      it 'responds with input json' do
+        response.body.should eq(@input.to_json)
+      end
     end
 
     context 'as non-owner' do
+      before do
+        sign_in FactoryGirl.create(:another_user)
+        get :show, format: :json, emulator_project_id: @project, simulator_specification_id: @spec, id: @input.name
+      end
+
+      it 'does not assign an input' do
+        assigns(:input).should be_nil
+      end
+
+      it 'does not contain a response body' do
+        response.body.should be_empty
+      end
+
       it 'responds with 401' do
-        # get :show, id: 
-        response.response_code.should == 401
+        response.response_code.should eq(401)
+      end
     end
   end
 
-  describe 'PUT #update'
+  describe 'PUT #update' do
     context 'as owner' do
     end
 
